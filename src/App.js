92c89c7ba92c9436e269ab9343fe7ab5c54ab7b9
BlooMethLabs <bmca620@gmail.com>
2022-05-02 13:03:09 +0100

    Cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
-  const expItems = [
+  const expenses = [
     { title: 'Car', date: new Date(2022, 4, 8), price: '12999' },
     { title: 'Bike', date: new Date(2012, 9, 18), price: '4599' },
     { title: 'Truck', date: new Date(2014, 12, 29), price: '18999' },
   ];
   return (
     <div>
-      {expItems.map((expItem) => (
-        <ExpenseItem
-          title={expItem.title}
-          date={expItem.date}
-          price={expItem.price}
-        ></ExpenseItem>
-      ))}
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import Card from './Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function Expenses(props) {
+  const expenses = props.items.map((item) => (
+      <ExpenseItem
+        title={item.title}
+        date={item.date}
+        price={item.price}
+      />
+    ));
+  return (
+    <Card className='expenses'>
+      {expenses}
+    </Card>
+  )
+}
+
+export default Expenses;
