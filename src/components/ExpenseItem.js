33f78caf2ed32ea11f0ae366de1b9efee12f12b0
BlooMethLabs <bmca620@gmail.com>
2022-05-02 12:21:48 +0100

    ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const date = props.date.toISOString();
   return (
     <div className="expense-item">
-      <div>{date}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__dexcription">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.price}</div>
